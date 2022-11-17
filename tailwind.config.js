a0399977b37cccd19f1d9eb81e31be661c84acf7
padrone1225 <lidinfomaborisovich@gmail.com>
2022-11-18 04:46:23 +0900

    complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-bg": "hsl(217, 54%, 11%)",
+        "card-bg": "hsl(216, 50%, 16%)",
+        "dark-blue": "hsl(215, 32%, 27%)",
+        "soft-blue": "hsl(215, 51%, 70%)",
+        Cyan: "hsl(178, 100%, 50%)",
+      },
+    },
   },
   plugins: [],
 };
